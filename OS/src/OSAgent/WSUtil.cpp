a4416542e91624d1d4dc8990cc8dab854a91c125
Kipp Martin <kmartin@chicagobooth.edu>
2006-08-14 19:37:27 +0000

    Edit config.ac to define WIN_SOCK

--- OS/src/OSAgent/WSUtil.cpp
+++ OS/src/OSAgent/WSUtil.cpp
@@ -15,11 +15,16 @@
  */ 
  
 #include "WSUtil.h"
+
+
 #include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
 #include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+#include <unistd.h>     /* for close() */
+
+
 #include <stdlib.h>     /* for atoi() and exit() */
 #include <string.h>     /* for memset() */
-#include <unistd.h>     /* for close() */
+
 #include <netdb.h>
 #include <iostream>
 #include <sstream> 
