1c246942bf192b0cc4990064ac72ffc66eb26eaa
Kipp Martin <kmartin@chicagobooth.edu>
2006-11-17 09:04:47 +0000

    update osiTest

--- OS/examples/osiTest/osiTest.cpp
+++ OS/examples/osiTest/osiTest.cpp
@@ -11,28 +11,28 @@ using std::endl;
 
 
 // this is the virtual class that all command classes inherite from
-class ModOSInstance {
+class OsiXForm {
 	public:
 	// build a queue of operations to perform
-	virtual void accumulate() = 0;
+	virtual void record() = 0;
 	// this method will execute the operations that have been accumulated in the queue
 	virtual void modify() = 0;
 	// opid is an integer that identifies each kind of operation, e.g. delete a constraint,
 	// add a variable etc.
 	int opid;
 	// the constructor and destructor
-	 ModOSInstance(){}
-	virtual	~ModOSInstance(){}
-}; //class ModOSInstance
+	 OsiXForm(){}
+	virtual	~OsiXForm(){}
+}; //class OsiXForm
 
 // an example command class, delete a bunch of constraints
-class DeleteConstraints : public ModOSInstance{
+class DeleteConstraints : public OsiXForm{
 	private:
 	int conNumber;
 	// this vector holds the list of constraints to delete
 	static std::vector<int> conNums;
 	public:
-	void accumulate( ){
+	void record( ){
 		conNums.push_back( conNumber);
 		cout << "Accumulating Constraint with Index =  " << conNumber << endl;
 	}
@@ -58,14 +58,14 @@ class DeleteConstraints : public ModOSInstance{
 std::vector<int> DeleteConstraints::conNums;
 
 // an example class, add a bunch of variables
-class AddVariables : public ModOSInstance{
+class AddVariables : public OsiXForm{
 	private:
 	int varNumber;
 	// a vector that holds the indicies of variables to add
 	static std::vector<int> varNums;
 	//
 	public:
-	void accumulate( ){
+	void record( ){
 		varNums.push_back( varNumber);
 		cout << "Accumulating a Variable with Index =  " << varNumber << endl;
 	}
@@ -92,44 +92,46 @@ class AddVariables : public ModOSInstance{
 // initialize static member conNums
 std::vector<int> AddVariables::varNums;
 
-class RunAccumulate {
-	// the vector accum stores operations used to modify the instance
-	std::vector<ModOSInstance*> accum;
+class OsiXFormRec {
+	// the vector modObjects stores operations used to modify the instance
+	std::vector<OsiXForm*> modObjects;
 	public:
-	std::map<int, ModOSInstance*> modObjectsMap;
+	std::map<int, OsiXForm*> modObjectsMap;
 	// add an object modifier
-	void add( ModOSInstance* osmod){
-		accum.push_back( osmod);
+	void record( OsiXForm* osmod){
+		modObjects.push_back( osmod);
 	}//add
-	//run the accumulate method in each operation
-	void runaccumulate(){
-		std::vector<ModOSInstance*>::iterator iter =  accum.begin();
-		while(iter != accum.end()){
-			(*iter)->accumulate();
-			// build a map of object operations that being performed
-			// if the object is not in the map, add ii
+	//run the record method in each operation
+	void callModClasses(){
+		std::vector<OsiXForm*>::iterator iter =  modObjects.begin();
+		while(iter != modObjects.end()){
+			(*iter)->record();
+			// build a map of object modifications/transormations
+			// we use this map to record which modification classes have been used
+			// the key is the opid of each modification class
+			// if the object is not in the map, add it
 			if( modObjectsMap.find( (*iter)->opid) == modObjectsMap.end() ){
 				modObjectsMap[ (*iter)->opid] = *iter;
 			}
 			iter++;			
 		}
 	}
-};// class RunAccumlate
+};// class OsiXFormRec  
 
 int main(int argC, char* argV[])
 {
-	std::map<int, ModOSInstance*>::iterator objectPos;
-	RunAccumulate accumulate;
+	std::map<int, OsiXForm*>::iterator objectPos;
+	OsiXFormRec modification;
 	// delete some constraints and add some variables
-	accumulate.add( new DeleteConstraints( 11) );
-	accumulate.add( new DeleteConstraints( 19) );
-	accumulate.add( new AddVariables( 1010) );
-	accumulate.add( new DeleteConstraints( 37) );
-	accumulate.add( new AddVariables( 20019) );
-	accumulate.runaccumulate();
+	modification.record( new DeleteConstraints( 11) );
+	modification.record( new DeleteConstraints( 19) );
+	modification.record( new AddVariables( 1010) );
+	modification.record( new DeleteConstraints( 37) );
+	modification.record( new AddVariables( 20019) );
+	modification.callModClasses();
   
 	
-for(objectPos = accumulate.modObjectsMap.begin(); objectPos != accumulate.modObjectsMap.end(); ++objectPos){
+for(objectPos = modification.modObjectsMap.begin(); objectPos != modification.modObjectsMap.end(); ++objectPos){
 		(objectPos->second)->modify();
 	}
 	return 0;
