e4dcb3f97717ffc7a67d933446acc258c85036d8
Jun Ma <majxuh@hotmail.com>
2006-12-06 04:19:45 +0000

    IPOPTSolver.java and IPOPTSolverService.java

---
+++ OSSolver/src/org/optimizationservices/ossolver/solver/IPOPTSolver.java
@@ -0,0 +1,164 @@
+/**
+ * @(#)IPOPTSolver 1.0 03/14/2004
+ *
+ * Copyright (c) 2004
+ */
+package org.optimizationservices.ossolver.solver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.GregorianCalendar;
+import java.util.Vector;
+
+import org.optimizationservices.oscommon.localinterface.DefaultSolver;
+import org.optimizationservices.oscommon.representationparser.OSrLWriter;
+import org.optimizationservices.oscommon.util.IOUtil;
+import org.optimizationservices.oscommon.util.OSParameter;
+import org.optimizationservices.oscommon.util.OSServiceUtil;
+import org.optimizationservices.oscommon.util.ProcessUtil;
+/**
+*
+* <P>The <code>IPOPTSolver</code>class is the real IPOPT computing engine. 
+*
+* </p>
+* @author Robert Fourer, Jun Ma, Kipp Martin
+* @version 1.0, 03/14/2004
+* @since OS 1.0
+*/
+public class IPOPTSolver extends DefaultSolver{
+
+	/**
+	 * default constructor.
+	 */
+	public IPOPTSolver(){
+	}//constructor
+
+	/**
+	 * run the IPOPT solver command that is specified in the batch file. 
+	 */
+	public void solve(){
+		String sOS = System.getProperty("os.name").toLowerCase();
+		String sJobID = osOption.getJobID();
+
+		String sInstanceFile = OSParameter.TEMP_FILE_FOLDER+sJobID+".osil";
+		if(IOUtil.existsFileOrDir(sInstanceFile)){
+			IOUtil.writeStringToFile(osil, sInstanceFile);
+		}
+		String sOptionFile = OSParameter.TEMP_FILE_FOLDER+sJobID+".osol";
+		if(IOUtil.existsFileOrDir(sOptionFile)){
+			IOUtil.writeStringToFile(osol, sOptionFile);
+		}
+
+		String sResultFile = OSParameter.TEMP_FILE_FOLDER+sJobID+".osrl";
+		OSrLWriter osrlWriter = new OSrLWriter();
+		osrlWriter.setServiceURI(OSParameter.SERVICE_URI);
+		osrlWriter.setServiceName(OSParameter.SERVICE_NAME);
+		osrlWriter.setResultTime(new GregorianCalendar());
+
+		//change starts
+		String sSolverPath = OSParameter.CODE_HOME + "solver/OSSolverService";
+		if(sOS.indexOf("window") >= 0){
+			sSolverPath += "Win32.exe";
+		}
+		else if(sOS.indexOf("linux") >= 0){
+			sSolverPath += "Linux";
+		}
+		else if(sOS.indexOf("mac") >= 0){
+			sSolverPath += "Mac";
+		}
+		else{
+			;
+		}
+
+		String sSolver = "ipopt"; 
+		String sArguments = 
+			" -osil " + sInstanceFile + 
+			" -osol " + sOptionFile +
+			" -osrl " + sResultFile + 
+			" -solver " + sSolver;
+
+		String[] msCommandLine = {
+				sSolverPath, 
+				"-osil", sInstanceFile, 
+				"-osol", sOptionFile,
+				"-osrl", sResultFile,
+				"-solver", sSolver};
+
+		//no need to change below
+		boolean bUseSimple;
+		if(OSParameter.SERVICE_URI == null || OSParameter.SERVICE_URI.length() <= 0){
+			bUseSimple = true;
+		}
+		else{
+			bUseSimple = false;
+		}
+		if(bUseSimple){//simple version - not registering process 
+			try {
+				ProcessUtil.launchAndWaitForFinish(sSolverPath + " " + sArguments);
+				super.osrl = IOUtil.readStringFromFile(sResultFile);				
+			} 
+			catch (Exception e) {
+				IOUtil.log(IOUtil.exceptionStackToString(e), null);
+				osrlWriter.setGeneralStatusType("error");
+				osrlWriter.setGeneralStatusDescription(e.getMessage());
+				super.osrl = osrlWriter.writeToString();
+			}
+		}
+		else{ //complex version - registering process
+			try{
+				Process process = null;		
+				process = Runtime.getRuntime().exec(msCommandLine);		
+				Vector<Process> vProcess = (Vector<Process>)OSServiceUtil.processsHashTable.get(sJobID);
+				if(vProcess == null || vProcess.size() <= 0){
+					vProcess = new Vector<Process>();
+				}
+				vProcess.add(process);
+				OSServiceUtil.processsHashTable.put(sJobID, vProcess);
+				BufferedReader br = new BufferedReader (new InputStreamReader(process.getInputStream ()));
+				boolean bCont = true;
+				String sProcessOutput = "";
+				String sTemp;
+				while (bCont){
+					sTemp = br.readLine(); 
+					if(sTemp != null){
+						sProcessOutput += sTemp;							
+					}
+					else{
+						bCont = false;
+					}
+				}
+				process.waitFor();
+				vProcess.remove(process);
+				if(vProcess.size() <= 0){
+					OSServiceUtil.processsHashTable.remove(sJobID);
+				}
+				br.close();	
+				super.osrl = IOUtil.readStringFromFile(sResultFile);
+			}
+			catch(Exception e){
+				IOUtil.log(IOUtil.exceptionStackToString(e), null);
+				osrlWriter.setGeneralStatusType("error");
+				osrlWriter.setGeneralStatusDescription(e.getMessage());
+				super.osrl = osrlWriter.writeToString();
+			}
+		}
+		if(OSParameter.SERVICE_URI == null || OSParameter.SERVICE_URI.length() <= 0){
+			IOUtil.deleteFile(sInstanceFile);
+			IOUtil.deleteFile(sOptionFile);
+			IOUtil.deleteFile(sResultFile);
+		}
+		return;
+	}//solve
+	
+	/**
+	 * main for test purposes.
+	 *
+	 * </p>
+
+	 * @param argv command line arguments.
+	 */
+	public static void main(String[] argv){
+		
+	}//main
+
+}//IPOPTSolver
