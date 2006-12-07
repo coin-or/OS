fe6c3e53b8b12c7b8dbc5af7b5c44fe05e7c5593
Jun Ma <majxuh@hotmail.com>
2006-12-07 02:07:06 +0000

    os.bat os2.bat drive changed to d: drive

--- OSCommon/util/os.bat
+++ OSCommon/util/os.bat
@@ -1,13 +1,13 @@
-rmdir "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes\org\optimizationservices" /S/Q
-xcopy "C:\code\java\OSjava\OSAnalyzer\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSCommon\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSMain\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSModeler\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSRegistry\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSScheduler\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSSimulation\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSSolver\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\OSCommon\lib" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\lib" /E/Y
-xcopy "C:\code\java\OSjava\Impact\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
-xcopy "C:\code\java\OSjava\Impact\lib" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\lib" /E/Y
-rmdir "C:\Program Files\Apache Software Foundation\Tomcat 5.02\webapps\os\WEB-INF\classes\package cache" /S/Q
+rmdir "D:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes\org\optimizationservices" /S/Q
+xcopy "D:\code\java\OSjava\OSAnalyzer\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSCommon\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSMain\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSModeler\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSRegistry\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSScheduler\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSSimulation\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSSolver\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\OSCommon\lib" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\lib" /E/Y
+xcopy "D:\code\java\OSjava\Impact\bin" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\classes" /E/Y
+xcopy "D:\code\java\OSjava\Impact\lib" "C:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps\os\WEB-INF\lib" /E/Y
+rmdir "D:\Program Files\Apache Software Foundation\Tomcat 5.02\webapps\os\WEB-INF\classes\package cache" /S/Q
