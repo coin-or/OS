df657ead6f9c33a64bc8e79de629e2264d31ae6f
Kipp Martin <kmartin@chicagobooth.edu>
2007-07-07 07:12:16 +0000

    add debugging to LindoSolver

--- OS/src/OSSolverInterfaces/LindoSolver.cpp
+++ OS/src/OSSolverInterfaces/LindoSolver.cpp
@@ -108,15 +108,19 @@ cout << "Lindo destructor called" << endl;
 	m_mcVarType = NULL;
     m_mcRowType = NULL;
 	m_msConName = NULL;
+	cout << "delete m_miSlackIdx" << endl;
 	delete[] m_miSlackIdx;
     m_miSlackIdx = NULL;
+    cout << "delete m_mdRhsValue" << endl;
 	delete[] m_mdRhsValue; 
 	m_mdRhsValue = NULL;
+	cout << "delete osrlwriter" << endl;
 	delete osrlwriter;
 	osrlwriter = NULL;
 #ifdef DEBUG
 	cout << "Delete LSdelete" << endl;
 #endif
+cout << "delete LSdeleteEvn" << endl;
 	LSdeleteEnv(&pEnv_);
 }
 
