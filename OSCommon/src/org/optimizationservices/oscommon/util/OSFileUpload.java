d1e8797e9b1361581b0a46d3fb62064a0a308cd7
Kipp Martin <kmartin@chicagobooth.edu>
2007-07-20 05:12:55 +0000

    update OSParameters

--- OSCommon/src/org/optimizationservices/oscommon/util/OSFileUpload.java
+++ OSCommon/src/org/optimizationservices/oscommon/util/OSFileUpload.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,7 +20,11 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 public class OSFileUpload extends HttpServlet{
+	public void init(ServletConfig config) throws ServletException {
+	}
+	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+		OSParameterFile.NAME = OSParameterFile.setParameterFile(request);
 		// first check if the upload request coming in is a multipart request
 		//boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 
@@ -42,25 +47,26 @@ public class OSFileUpload extends HttpServlet{
 			String fileName = "";
 			item = (FileItem) fileItems.get( 0);
 			fileName = item.getName();
-			String path = "/home/kmartin/temp/";
+			String path = OSParameter.TEMP_FILE_FOLDER;//"/home/kmartin/temp/";
 			File uploadedFile = new File(path + fileName);
 			try{
 			item.write( uploadedFile);
 			}
 			catch(java.lang.Exception ioe){
 				out.println("<html>");
-				out.println( "there was an error trying to write the uploaded file");
+				out.println( "There was an error trying to save the uploaded file.");
 				out.println("</html>");		
                 out.println("\r\n");	
 			}
             out.println("<html>");
-			out.println( fileName );
+			out.println( "The file " + fileName + " was successfuly uploaded and saved on the following server directory:");
+			out.println( OSParameter.TEMP_FILE_FOLDER+fileName);
 			out.println("</html>");
             out.println("\r\n");
 		}
 		catch(FileUploadException fe) {
 			out.println("<html>");
-			out.println( "there was an error getting the file" );
+			out.println( "There was an error getting the file." );
             out.println(  fe );
 			out.println("</html>");
             out.println("\r\n");
--- OSCommon/src/org/optimizationservices/oscommon/util/OSParameterFile.java
+++ OSCommon/src/org/optimizationservices/oscommon/util/OSParameterFile.java
@@ -5,6 +5,12 @@
  */
 package org.optimizationservices.oscommon.util;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.axis.MessageContext;
+import org.apache.axis.transport.http.HTTPConstants;
 /**
 *
 * <P>The <code>OSParameterFile</code> class stores OSParameter file related information.  
@@ -29,4 +35,61 @@ public final class OSParameterFile {
 	 */
 	public static String NAME = "/code/OSConfig/OSParameter.xml"; //IOUtil.getCurrentDir()+"webapps/os/code/OSParameter.xml"; 
 	
+
+	/**
+	 * @return the parameter file path name the class sets. 
+	 */
+	public static String setParameterFile(){
+		return setParameterFile(null);
+	}
+	
+	/**
+	 * @return the parameter file path name the class sets. 
+	 */
+	public static String setParameterFile(HttpServletRequest httpRequest){
+		//automatically detect webapp context
+		String SERVICE_FOLDER = "os";		
+		
+		MessageContext messageContext = MessageContext.getCurrentContext();
+		HttpServletRequest request = null;
+		if(httpRequest == null){
+			request = (HttpServletRequest)messageContext.getProperty(HTTPConstants.MC_HTTP_SERVLETREQUEST);
+		}
+		else{
+			request = httpRequest;
+		}
+		SERVICE_FOLDER = request.getContextPath();
+		if(SERVICE_FOLDER != null && SERVICE_FOLDER.length() > 0)SERVICE_FOLDER = SERVICE_FOLDER.substring(1);
+		if(SERVICE_FOLDER == null || SERVICE_FOLDER.length() <= 0) SERVICE_FOLDER = "os";		
+
+		//automatically detect where the tomcat installation is. 
+		String sDir = "";
+		try {
+			File dir = new File (".");
+			sDir = dir.getCanonicalPath();
+			sDir = sDir.replace('\\', '/');
+			if(!sDir.endsWith("/")){
+				sDir += "/";
+			}
+		}
+		catch(Exception e) {
+			sDir = "/Program Files/Apache Software Foundation/Tomcat 5.5/";
+		}
+		
+		
+		
+		if(sDir.toLowerCase().endsWith("bin") || 
+				sDir.toLowerCase().endsWith("bin\\") ||
+				sDir.toLowerCase().endsWith("bin/")){
+			int iIndex = sDir.lastIndexOf("bin");
+			sDir = sDir.substring(0, iIndex);
+		}
+		if(!sDir.endsWith("/")){
+			sDir += "/";
+		}
+
+		NAME = sDir+= "webapps/"+SERVICE_FOLDER+"/WEB-INF/code/OSConfig/OSParameter.xml";
+		return NAME;
+		
+	}//setParameterFile
 }//OSParameterFile
