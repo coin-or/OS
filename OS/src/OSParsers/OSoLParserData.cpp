101da950a91fd093205dabd913e6c1eb2def7ec2
Gus Gassmann <horand.gassmann@dal.ca>
2008-09-05 20:05:38 +0000

    Added OSoLParserData.cpp

---
+++ OS/src/OSParsers/OSoLParserData.cpp
@@ -0,0 +1,29 @@
+/** @file OSoLParserData.cpp
+ * 
+ * @author  Robert Fourer, Horand Gassmann, Jun Ma, Kipp Martin, 
+ * @version 1.0, 10/05/2005
+ * @since   OS1.0
+ *
+ * \remarks
+ * Copyright (C) 2005, Robert Fourer, Horand Gassmann, Jun Ma, Kipp Martin,
+ * Northwestern University, Dalhousie University and the University of Chicago.
+ * All Rights Reserved.
+ * This software is licensed under the Common Public License. 
+ * Please see the accompanying LICENSE file in root directory for terms.
+ * 
+ */
+ #include "OSoLParserData.h" 
+
+ OSoLParserData::~OSoLParserData() {
+
+ }//~OSoLParserData
+ 
+
+ OSoLParserData::OSoLParserData() :
+ 	statusType(""),
+	statusDescription(""),
+	errorText(NULL)
+ {
+
+ }//OSrLParserData
+ 
