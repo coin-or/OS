a8e3ceb43c33e04719d6eb4d69e51f24d5070735
Kipp Martin <kmartin@chicagobooth.edu>
2008-11-25 05:17:09 +0000

    edit parsertest.osol

--- OS/src/OSParsers/OSParseosol.tab.cpp
+++ OS/src/OSParsers/OSParseosol.tab.cpp
@@ -435,7 +435,7 @@ typedef union YYSTYPE
 	int ival;
 	char* sval;
 }
-/* Line 187 of yacc.c.  */
+/* Line 193 of yacc.c.  */
 
 	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
--- OS/src/OSParsers/OSParseosrl.tab.cpp
+++ OS/src/OSParsers/OSParseosrl.tab.cpp
@@ -266,7 +266,7 @@ typedef union YYSTYPE
 	int ival;
 	char* charval;
 }
-/* Line 187 of yacc.c.  */
+/* Line 193 of yacc.c.  */
 
 	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
