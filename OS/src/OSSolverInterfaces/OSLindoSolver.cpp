19ab5acacab72c440332848ec86e2e52dc7df55f
Kipp Martin <kmartin@chicagobooth.edu>
2009-10-20 05:01:44 +0000

    fix problem for Stefan

--- OS/src/OSSolverInterfaces/OSLindoSolver.cpp
+++ OS/src/OSSolverInterfaces/OSLindoSolver.cpp
@@ -118,6 +118,7 @@ LindoSolver::~LindoSolver() {
 	//delete[] m_mdVarLB ;
 	//delete[] m_mdVarUB ;
 	delete[] m_mmcVarName ;
+	//delete[] m_mcVarType;
 	m_mdConLB = NULL;
 	m_mdConUB = NULL;
 	m_msVarName = NULL;
@@ -352,7 +353,13 @@ bool LindoSolver::addSlackVars(){
 bool LindoSolver::processVariables(){
 	int i;
 	try{
-		m_mcVarType = osinstance->getVariableTypes();
+		m_mcVarType = new char[ osinstance->getVariableNumber()];
+		for(i = 0; i <  osinstance->getVariableNumber() ; i++){
+			//m_mcVarType[ i] =	osinstance->getVariableTypes()[ i];
+			m_mcVarType[ i] = 'C';
+			//if(	osinstance->getVariableTypes()[ i] == 'B') m_mcVarType[ i] = 'B';
+			if(	osinstance->getVariableTypes()[ i] == 'I') m_mcVarType[ i] = 'I';
+		}		
 		m_mdVarLB = osinstance->getVariableLowerBounds();
 		m_mdVarUB  = osinstance->getVariableUpperBounds();
 		m_mmcVarName = new char*[ osinstance->getVariableNumber()];
@@ -362,6 +369,17 @@ bool LindoSolver::processVariables(){
 			//m_mmcVarName[i] = p;
 			m_mmcVarName[i] = &osinstance->getVariableNames()[  i][0];
 		}
+		
+		for(i = 0; i <  osinstance->getVariableNumber() ; i++){
+			//m_mcVarType[ i] =	osinstance->getVariableTypes()[ i];
+			m_mcVarType[ i] = 'C';
+			if(	osinstance->getVariableTypes()[ i] == 'B'){
+				m_mcVarType[ i] = 'I';
+				m_mdVarUB[ i] = 1.0;
+			}
+			if(	osinstance->getVariableTypes()[ i] == 'I') m_mcVarType[ i] = 'I';
+		}
+		
 		return true;
 	} 
 	catch(const ErrorClass& eclass){
