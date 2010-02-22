1b0d0c61e231e0897bda0ed168f70e2faa5b254b
Kipp Martin <kmartin@chicagobooth.edu>
2010-02-22 07:57:33 +0000

    fix Makefile.in on all examples

--- OS/examples/osSolverDemo/OSSolverDemo.cpp
+++ OS/examples/osSolverDemo/OSSolverDemo.cpp
@@ -409,8 +409,10 @@ int main( ){
 		* Create a result object and get the optimal objective
 		* and primal variable values
 		*/	
+		std::cout  << "call get osresult" << std::endl;
+		std::cout << solver->osrl << std::endl;
 		getOSResult( solver->osrl);
-
+		std::cout  << "finish call get osresult" << std::endl;
 		// start garbage collection
 		delete osilreader;
 		osilreader = NULL;
