3f5526a0fbf92e534032450f39e8c93d3c026b2f
mtim <mtim@coin-or.clemson.edu>
2010-04-03 20:41:58 +0000

    Windows/Externals build.

---
+++ OSServer/test/test.cmd
@@ -0,0 +1,2 @@
+..\..\OSLib\lib\coin-or\OSSolverService-Windows-x86 -osil first.osil -osrl first.osrl -serviceMethod solve -serviceLocation http://localhost:8080/OSServer/services/OSSolverService
+pause
