e0dd6763682844da9557e50cfbb306d38e06d13b
mtim <mtim@coin-or.clemson.edu>
2010-04-08 12:53:08 +0000

    Test based on local server binary in lib.

--- OSServer/test/test.sh
+++ OSServer/test/test.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-OSSolverService -osil first.osil -osrl first.osrl -serviceMethod solve -serviceLocation http://localhost:8080/OSServer/services/OSSolverService
+ARCH="$(uname -s)-$(uname -m)"
+: ${OSSOLVERSERVICE:=../../OSLib/lib/coin-or/OSSolverService-${ARCH}}
+: ${PORT:=8080}
+
+${OSSOLVERSERVICE} -osil first.osil -serviceMethod solve -serviceLocation http://localhost:${PORT}/OSServer/services/OSSolverService
