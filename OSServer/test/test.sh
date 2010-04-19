54b3ff05af9a610f281dc4ba2f100b4cacd855af
mtim <mtim@coin-or.clemson.edu>
2010-04-19 13:23:26 +0000

    Mac arch changes

--- OSServer/test/test.sh
+++ OSServer/test/test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 ARCH="$(uname -s)-$(uname -m)"
+case ${ARCH} in
+Darwin-i386)
+    ARCH=Mac-x86_64
+    ;;
+esac
+
 : ${OSSOLVERSERVICE:=../../OSThirdParty/lib/coin-or/OSSolverService-${ARCH}}
 : ${PORT:=8080}
 
