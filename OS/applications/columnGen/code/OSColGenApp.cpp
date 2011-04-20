51e2fe29749787b121d490bdb2c8bb58d5fe69ac
Kipp Martin <kmartin@chicagobooth.edu>
2011-04-20 05:22:41 +0000

    work on column generation

--- OS/applications/columnGen/code/OSColGenApp.cpp
+++ OS/applications/columnGen/code/OSColGenApp.cpp
@@ -379,7 +379,7 @@ void OSColGenApp::solve(){
 		//print LP value at node
 		std::cout <<  "optimal LP value at root node = "  <<  m_zLB << std::endl;
 		
-		exit( 1);
+		//exit( 1);
 
 		for ( sit = m_osrouteSolver->intVarSet.begin() ; 
 				sit != m_osrouteSolver->intVarSet.end(); sit++ ){
