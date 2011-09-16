8359fe3e5e9a727f2f04d7d5c4056ed118c8e275
Gus Gassmann <horand.gassmann@dal.ca>
2011-09-16 11:12:26 +0000

    Changed applications code to Eclipse Public License

--- OS/applications/fileUpload/OSFileUpload.cpp
+++ OS/applications/fileUpload/OSFileUpload.cpp
@@ -5,10 +5,10 @@
  * @since   OS1.0
  *
  * \remarks
- * Copyright (C) 2005-2010, Robert Fourer, Gus Gassmann, Jun Ma, Kipp Martin,
+ * Copyright (C) 2005-2011, Robert Fourer, Gus Gassmann, Jun Ma, Kipp Martin,
  * Northwestern University, Dalhousie University and the University of Chicago.
  * All Rights Reserved.
- * This software is licensed under the Common Public License. 
+ * This software is licensed under the Eclipse Public License. 
  * Please see the accompanying LICENSE file in root directory for terms.
  * 
  * <p>
