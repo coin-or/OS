d1f3af276bb845f1b0180ada81d04740f706f8f1
Stefan Vigerske <svigerske@gams.com>
2013-07-23 10:20:18 +0000

    fix version info

--- OS/src/config_os_default.h
+++ OS/src/config_os_default.h
@@ -5,13 +5,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define OS_VERSION      "2.6.0"
+#define OS_VERSION      "trunk"
 
 /* Major Version number of project */
-#define OS_VERSION_MAJOR   2
+#define OS_VERSION_MAJOR   9999
 
 /* Minor Version number of project */
-#define OS_VERSION_MINOR   6
+#define OS_VERSION_MINOR   9999
 
 /* Release Version number of project */
 #define OS_VERSION_RELEASE 9999
