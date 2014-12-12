1326f365c1649d536ac5fc2a021c5e3519b6c287
Gus Gassmann <horand.gassmann@dal.ca>
2014-12-12 12:35:22 +0000

    restoring OS/stable/2.8

--- OS/src/config_os_default.h
+++ OS/src/config_os_default.h
@@ -5,16 +5,16 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define OS_VERSION "2.8.4"
+#define OS_VERSION      "2.8"
 
 /* Major Version number of project */
-#define OS_VERSION_MAJOR 2
+#define OS_VERSION_MAJOR   2
 
 /* Minor Version number of project */
-#define OS_VERSION_MINOR 8
+#define OS_VERSION_MINOR   8
 
 /* Release Version number of project */
-#define OS_VERSION_RELEASE 4
+#define OS_VERSION_RELEASE 9999
 
 /* Define to 1 if OS uses CppAD */
 #define OS_HAS_CPPAD
